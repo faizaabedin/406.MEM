b62a9ec9e74c8c94e8216ffa8c02ca9c35e989be
faizaabedin <33582390+faizaabedin@users.noreply.github.com>
2018-03-17 21:27:28 -0400

    Add files via upload

--- coach.java
+++ coach.java
@@ -9,6 +9,7 @@ public class coach {
 	private String name, className;
 	private Member member;
 	private Lesson lesson;
+	private Lesson lesson2;
 	private LinkedList MemberList;
 	private LinkedList NotPaidList;
 	private LinkedList lessonList;
@@ -20,24 +21,31 @@ public class coach {
 		this.name = name;
 		this.className = className;
 		MemberList = new LinkedList();
-		//lesson = new Lesson();
+		NotPaidList = new LinkedList();
+		lessonList = new LinkedList();
+		lesson = new Lesson();
+		lesson2 = new Lesson();
 	}
 	public void addMember(String name, String phoneNumber, boolean paid, String address){
 		member = new Member(name,phoneNumber,paid,address);
+		
 		MemberList.add(member);
 		
-		if (!member.isPaid()){
+		if (member.paid == false){
 			NotPaidList.add(member);
 		}
+		
 	}
-	public void scheduleClass(int month, int date, int time, String message){
+	public void scheduleLesson(int month, int date, int time, String message){
 		lesson = new Lesson(month, date, time, message);
 		lessonList.add(lesson);
 		
 	}
-	public void cancelClass(int month, int date, int time, String message){
-		
+	public void cancelLesson(int month, int date, int time, String message){
+		lesson2 = new Lesson(month,date,time,message);
+		if(lesson.equals(lesson2)){
+			lessonList.remove(lesson2);
+		};
 	}
-
 	
 }
